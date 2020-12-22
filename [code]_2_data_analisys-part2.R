04562fb7e984382a92d7ad230ad96fdd17ecc3be
ParkWonBin <57087624+ParkWonBin@users.noreply.github.com>
2020-12-23 03:10:25 +0900

    워딩 수정

--- [code]_2_data_analisys-part2.R
+++ [code]_2_data_analisys-part2.R
@@ -71,3 +71,28 @@ data.frame(season = c('1~3월','4~6월','7~9월','10~12월','전체기간'),
            p.lmcoff   = c(a[8],b[8],c[8],d[8],e[8]),
            p.intercept= c(a[7],b[7],c[7],d[7],e[7]),
            Std.error  = c(a[3],b[3],c[3],d[3],e[3]))
+
+######################################################
+library(dplyr)     # %>% 함수 
+library(ggplot2)   # alpha 함수 
+library(lubridate) # month 함수
+
+head(Bike)
+head(Bike_vc)
+head(Brok_vc)
+Bike_vc$month = factor(month(Bike_vc$date))
+Brok_vc$month = factor(month(Brok_vc$date))
+
+
+par(mfcol=c(2,1),xaxt = "t", mar=c(2, 1, 2, 1),oma=c(0.5, 1.5, 0.5, 0.5))
+boxplot(Bike_vc$n ~ Bike_vc$month , col=rainbow(12))
+title('계절별 대여수',cex.main=2)
+boxplot(Brok_vc$n ~ Brok_vc$month , col=rainbow(12))
+title('계절별 고장수',cex.main=2)
+
+
+mtext('대여건수 1개당 (고장건수+52)의 값',side=3,line=-1.5,cex=1.5)
+mtext('anova 분석 결과 : F= 2.266, P=0.00979',side=1,cex=1.5,line=0.5)
+# par(mfcol=c(1,1),xaxt = "t")
+# summary(with(m1,aov((broken+52)/rental~month)))
+
